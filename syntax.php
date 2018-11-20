f2bdf8857d825937039394ec0910458a4e45ce3f
Andreas Gohr <gohr@cosmocode.de>
2018-11-20 11:18:41 +0100

    cleanup and signature fixes
    This cleans up the code and makes signatures match the parent classes.
    No real code refacoring has been done, yet.

--- syntax.php
+++ syntax.php
@@ -1,62 +1,59 @@
 <?php
+
 /**
  * DokuWiki Plugin shorturl (Syntax Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Frank Schiebel <frank@linuxmuster.net>
  */
+class syntax_plugin_shorturl extends DokuWiki_Syntax_Plugin
+{
 
-// must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
-
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once(DOKU_PLUGIN.'syntax.php');
-require_once(dirname(__FILE__) . "/action.php");
-
-class syntax_plugin_shorturl extends DokuWiki_Syntax_Plugin {
-
-    function syntax_plugin_shorturl() { }
-
-    function getType() {
+    /** @inheritdoc */
+    public function getType()
+    {
         return 'substition';
     }
 
-    function getPType() {
+    /** @inheritdoc */
+    public function getPType()
+    {
         return 'block';
     }
 
-    function getSort() {
+    /** @inheritdoc */
+    public function getSort()
+    {
         return 302;
     }
 
-    function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('\~\~SHORTURL\~\~',$mode,'plugin_shorturl');
+    /** @inheritdoc */
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('\~\~SHORTURL\~\~', $mode, 'plugin_shorturl');
     }
 
-    function handle($match, $state, $pos, &$handler){
-
-        $data['todo'] = "print";
+    /** @inheritdoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        $data['todo'] = 'print';
         return $data;
     }
 
-    function render($mode, &$renderer, $data) {
+    /** @inheritdoc */
+    public function render($mode, Doku_Renderer $renderer, $data)
+    {
         global $ID;
 
-        if($mode != 'xhtml') return false;
+        if ($mode !== 'xhtml') return false;
 
-        if ( $data['todo'] == "print" ) {
-            $shorturl =& plugin_load('helper', 'shorturl');
-            if ($shorturl) {
-                $shortID = $shorturl->autoGenerateShortUrl($ID);
-                $renderer->doc .= "<a href=". wl($shortID, "", true) ." class=\"shortlinkinpage\" >" . $this->getLang('shortlinktext')  . "</a>\n";
-            }
+        if ($data['todo'] === 'print') {
+            /** @var helper_plugin_shorturl $shorturl */
+            $shorturl = plugin_load('helper', 'shorturl');
+            $shortID = $shorturl->autoGenerateShortUrl($ID);
+            $renderer->doc .= '<a href=' . wl($shortID, '', true) . ' class="shortlinkinpage" >' . $this->getLang('shortlinktext') . "</a>\n";
         }
 
         return true;
     }
 }
-
-// vim:ts=4:sw=4:et:enc=utf-8:
